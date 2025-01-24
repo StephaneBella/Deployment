780380494a7703bd331a16cb86c68bf8e896bd34
StephaneBella <lestephendylan241@gmail.com>
2025-01-24 15:10:24 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -3,7 +3,8 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-#import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Page configuration
 st.set_page_config(
